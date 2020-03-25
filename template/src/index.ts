1f166562bc183d807cd8b8ea8a09a35b99a4d558
heynext <heynext.xyz@gmail.com>
2020-03-25 23:42:16 +0800

    chore(template): update template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,1 +1,25 @@
-export default {};
+#!/usr/bin/env node
+
+import program from "commander";
+import chalk from "chalk";
+
+import init from "./lib/init";
+
+program
+  .version(`<%= name %> ${require("../package.json").version}`)
+  .usage("<command> [options]");
+
+program
+  .command("init [dir]")
+  .description("<%= description %>")
+  .option("-d, --dest <dest>", "dest")
+  .action(async (dir, options) => {
+    console.log(chalk.blueBright("   to be start\n"));
+    console.log({
+      dir,
+      dest: options.dest,
+    });
+    await init();
+  });
+
+program.parse(process.argv);
